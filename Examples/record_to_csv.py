c17bd54c24633c4037f507f5c777b98f2d311552
Heinrich <freelanceharms@gmail.com>
2020-12-07 15:28:48 -0600

    commented and revised record_to_csv.py

--- Examples/record_to_csv.py
+++ Examples/record_to_csv.py
@@ -2,25 +2,38 @@ from AlbiPy import sniffing_thread
 from AlbiPy import HEADERS
 from time import sleep
 
+# initialize and start sniffing thread
 print("Starting sniffing thread...\nHit ctrl-c to stop recording and save results!")
 thread = sniffing_thread()
 thread.start()
 
-running = True
+# wait forever until keyboard interupt is detected
 try:
-    sleep(72000000)
+    while True: pass
 except KeyboardInterrupt:
     pass
 
+# stop sniffing thread
 thread.stop()
 print("\nThread stopped!")
+
+# fetch captured data and store in orders
 orders = thread.get_data()
 
+# get output filename from user
 output_filename = input("Output csv filename: ")
+
 print("Writing data to csv...")
+# open file
 output_file = open(output_filename, "w")
+
+# write headers to file
 output_file.write(",".join(HEADERS)+"\n")
+
+# write parsed datapoints to file
 for order in orders:
     output_file.write(",".join(list(map(str, order.data)))+"\n")
+
+# close output file
 output_file.close()
 print("Wrote", len(orders), "datapoints to", output_filename)
